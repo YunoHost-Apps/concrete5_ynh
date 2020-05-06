1fd8f2eef8c56b1a1eb3bf9309cde83d79e5a817
yalh76 <yalh@yahoo.com>
2020-05-06 21:31:24 +0200

    Upgrade to PHP7.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 #=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="curl"
+
+extra_pkg_dependencies="php7.3-pdo php7.3-mysql php7.3-dom php7.3-simplexml php7.3-iconv php7.3-gd php7.3-fileinfo php7.3-mbstring php7.3-curl php7.3-zip"
+
+#=================================================
 # PERSONAL HELPERS
 #=================================================
 
